e1e96da752545a1f25fa0a606d2b9a4c714de59c
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-15 16:59:22 +0100

    implement the vannkoker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    public class Vannkoker
+    {
+        private int roomTemp = 20;
+        private int maxTemp = 100;
+        public event EventHandler? TemperatureReached;
+
+        public void StartBoiling()
+        {
+            for (int i = roomTemp; i < maxTemp; i++)
+            {
+                string? message = i switch
+                {
+                    30 or 50 or 70 or 100 => "Water is boiling, current temperature reached: " + i,
+                    _ => null
+
+                };
+
+                if (message != null) Console.WriteLine(message);
+
+            }
+
+            BoilingComplete(EventArgs.Empty);
+        }
+
+        protected virtual void BoilingComplete(EventArgs e)
+        {
+            TemperatureReached?.Invoke(this, e);
+        }
+    }
+
+    public class Smartphone
+    {
+        public void PushNotification(object? sender, EventArgs e)
+        {
+            Console.WriteLine("The water has finished boiling!");
+        }
+    }
+
+    public class App
+    {
+        public static void Main()
+        {
+            Vannkoker norwegianKettle = new Vannkoker();
+            Smartphone googlePixel = new Smartphone();
+
+            norwegianKettle.TemperatureReached += googlePixel.PushNotification;
+
+            norwegianKettle.StartBoiling();
+
+        }
+    }
+}
